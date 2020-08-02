2fff74dc453c2846e34441f385358295c3dd5b5b
Pedro Ferreira <ddrppx@gmail.com>
2020-08-02 18:09:40 -0300

    class Connect

--- src/Database/Connect.php
+++ src/Database/Connect.php
@@ -1,14 +1,21 @@
 <?php 
     namespace Database;
+    use \PDO;
+    define('DS', DIRECTORY_SEPARATOR);
+
     class Connect {
         private static $instance;
+        const ROOT = __DIR__;
+        const pathDB = "database.sqlite";
 
         public static function getConn() {
-
-            // if (!isset(self::$instance)){
-            self::$instance = new \PDO('sqlite:database.sqlite');
-            // }
-            
+                //Verifica se ja tem ou não uma instancia do banco
+            if (!isset(self::$instance)){
+                self::$instance = new PDO('sqlite:'. self::ROOT. DS. self::pathDB);
+                //Modifica os atributos para o erro não ser silencioso
+                self::$instance -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+                //Retorna a instancia
             return self::$instance;
         }
     }
