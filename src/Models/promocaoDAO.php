7e268ce7e5bfdc17047096463f88a9a77e750899
Pedro Ferreira <ddrppx@gmail.com>
2020-08-02 22:51:38 -0300

    promocaoDAO

---
+++ src/Models/promocaoDAO.php
@@ -0,0 +1,68 @@
+<?php
+    namespace Models;
+    use Classes\Promocao;
+    use Database\Connect;
+    use \PDO;
+    use \PDOException;
+
+class cupomDAO {
+       
+        public function create(Promocao $promo) {
+            try {
+                    //Comando SQL
+                $sql = 'INSERT INTO promocao (codigo, desconto) VALUES (?, ?)';
+                    //Conexão com banco + prepare
+                $stmt = Connect::getConn() -> prepare($sql);
+                    //Agrega o valor ao local do '?' na variavel $sql
+                $stmt -> bindValue(1, $promo -> getNome(), PDO::PARAM_STR);
+                $stmt -> bindValue(2, $$promo -> getDesconto(), PDO::PARAM_INT);
+                    //Executa com os valores agregados
+                $stmt -> execute();
+            } catch (PDOException $e) {
+                $e -> getMessage();
+            }
+
+        }
+
+        public function read() {
+                //Comando SQL
+            $sql = 'SELECT * FROM promocao';
+                //Faz conexão à classe que retorna a instancia do banco
+            $stmt = Connect::getConn() -> prepare($sql);
+            $stmt -> execute();
+                //Variavel que ira retornar todas linhas do banco
+            $resultado = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+                //Retorno da variavel
+            return $resultado;
+        }
+
+        public function update(int $id, Promocao $promo) {
+                //Comando SQL
+            $sql = 'UPDATE promocao SET nome = ?, desconto = ? WHERE id = ?';
+                //Faz conexão à classe que retorna a instancia do banco
+            $stmt = Connect::getConn() -> prepare($sql);
+                //Agrega o valor ao local do '?' na variavel $sql
+            $stmt -> bindValue(1, $promo -> getNome(),PDO::PARAM_STR);
+            $stmt -> bindValue(2, $promo -> getDesconto(), PDO::PARAM_INT);
+            $stmt -> bindValue(3, $id);
+                //Executa o comando sql
+            $stmt -> execute();
+
+            echo "End update.";
+        }
+
+        public function delete(int $id) {
+                //Comando SQL
+            $sql = 'DELETE FROM promocao WHERE id = ?';
+                //Faz conexão à classe que retorna a instancia do banco
+            $stmt = Connect::getConn() -> prepare($sql);
+                //Agrega o valor ao local do '?' na variavel $sql
+            $stmt -> bindValue(1, $id);
+                //Executa o comando sql
+            $stmt -> execute();
+
+            echo "End delete.";
+        }
+
+    }
+?>
