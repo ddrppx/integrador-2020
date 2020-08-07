ef2ac0220a2bfa5e890678628c59a5b14f031b43
Pedro Ferreira <ddrppx@gmail.com>
2020-08-06 23:29:13 -0300

    carrinho example test

---
+++ carrinho.php
@@ -0,0 +1,36 @@
+<?php
+
+    require_once "./vendor/autoload.php";
+use Models\bebidaDAO;
+use Models\lancheDAO;
+
+session_start();
+
+$beb = new bebidaDAO;
+
+if (!isset($_SESSION['itens'])){
+    $_SESSION['itens'] = [];
+}
+
+if (isset($_GET['add']) && $_GET['add'] == "carrinho"){
+    $idProduto = $_GET['id'];
+    if(!isset($_SESSION['itens'][$idProduto])) {
+        $_SESSION['itens'][$idProduto] = 1;
+    } else {
+        $_SESSION['itens'][$idProduto] += 1;
+    }
+}
+
+if (count($_SESSION['itens']) == 0) {
+    echo 'Carrinho Vazio <br/> <a href="test.php"> Adicionar itens</a>'; 
+}else {
+    foreach ($_SESSION['itens'] as $idProduto => $quantidade) {
+        $prods = $beb -> readId($idProduto);
+        
+        echo "Nome: ". $prods[0]['nome']. "<br/>";
+        echo "Valor: ". $prods[0]['valor']. "<br/>";
+        echo "Quantidade ". $quantidade ."<br/>";
+        echo "Total: ". ($prods[0]['valor'] * $quantidade). "<hr/><br/>";
+        echo '<a href="remover.php?remover=carrinho&&id='. $idProduto.'"> Remover </a>'; 
+    } 
+}
