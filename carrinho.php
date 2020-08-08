12a6a5f7dac61e603236f9ac7c8aeba43240a30c
Pedro Ferreira <ddrppx@gmail.com>
2020-08-08 00:02:27 -0300

    carrinho modifications

--- carrinho.php
+++ carrinho.php
@@ -16,22 +16,57 @@ if (isset($_GET['add']) && $_GET['add'] == "carrinho"){
     $idProduto = $_GET['id'];
     if(!isset($_SESSION['itens'][$idProduto])) {
         $_SESSION['itens'][$idProduto] = 1;
-        $_SESSION['itens'][$idProduto]['classe'] = "Lanche";
         
     } else {
         $_SESSION['itens'][$idProduto] += 1;
     }
 }
 
-    echo "<pre>";
-    print_r($_SESSION['itens']);
+if (count($_SESSION['itens']) == 0) {
+    echo 'Carrinho Vazio <br/> <a href="test.php"> Adicionar itens</a>'; 
+}else {
+    echo "<pre";
+    var_dump($_SESSION['itens']);
     echo "</pre>";
+    foreach ($_SESSION['itens'] as $idProduto => $quantidade) {
+        $prods = $beb -> readId($idProduto);
+        
+        
+        echo "Nome: ". $prods[0]['nome']. "<br/>";
+        echo "Valor: ". $prods[0]['valor']. "<br/>";
+        echo "Quantidade ". $quantidade ."<br/>";
+        echo "Total: ". ($prods[0]['valor'] * $quantidade);
+        echo '<a href="remover.php?remover=carrinho&&id='. $idProduto.'"> Remover </a>'. "<hr/><br/>"; 
+    } 
+}
+
+    //Modificacoes
+if (!isset($_SESSION['itens'])){
+
+    $_SESSION['lanches'] = [];
+    $_SESSION['acomps'] = [];
+    $_SESSION['bebidas'] = [];
+}
+    //STOPPED HERE
+if (isset($_POST['add']) && $_POST['add'] == "carrinho"){
+    $idProduto = $_GET['id'];
+    if(!isset($_SESSION['itens'][$idProduto])) {
+        $_SESSION['itens'][$idProduto] = 1;
+        
+    } else {
+        $_SESSION['itens'][$idProduto] += 1;
+    }
+}
 
 if (count($_SESSION['itens']) == 0) {
     echo 'Carrinho Vazio <br/> <a href="test.php"> Adicionar itens</a>'; 
 }else {
+    echo "<pre";
+    var_dump($_SESSION['itens']);
+    echo "</pre>";
     foreach ($_SESSION['itens'] as $idProduto => $quantidade) {
         $prods = $beb -> readId($idProduto);
+        
         
         echo "Nome: ". $prods[0]['nome']. "<br/>";
         echo "Valor: ". $prods[0]['valor']. "<br/>";
