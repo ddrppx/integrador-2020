2f42538cef6209e8acb7128d803947a79abcbc8c
Pedro Ferreira <ddrppx@gmail.com>
2020-08-10 20:23:06 -0300

    cancelar.php done

--- src/Pages/cancelar.php
+++ src/Pages/cancelar.php
@@ -1,24 +1,14 @@
 <?php
     session_start();
-    
+        //Destroi as variaveis armazenando dados da sessao
+        //e retorna o usuario à pagina inicial
     if ($_GET['cancelar'] && $_GET['cancelar'] == 1) {
-        //     //Destrói as variaveis de sessao usadas/existentes
-        // unset($_SESSION['inexistente']);
-        // unset($_SESSION['preparo']);
-        // unset($_SESSION['pagamento']);
-        // unset($_SESSION['categoria']);
-        // unset($_SESSION['preco']);
-        // unset($_SESSION['lanches']);
-        // unset($_SESSION['bebidas']);
-        // unset($_SESSION['acomp']);
-
         session_unset();
         session_destroy();
         session_write_close();
         setcookie(session_name(),'',0,'/');
-        session_regenerate_id(true);
     }
 
     clearstatcache();
-    // echo '<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=../../index.php">';
+    echo '<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=../../index.php">';
 ?>
